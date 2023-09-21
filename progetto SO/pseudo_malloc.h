49e0932ba4b5d8e4c5f1368417c8812d07af0679
margheritamerialdo <130069689+margheritamerialdo@users.noreply.github.com>
2023-09-21 16:49:19 +0200

    Update pseudo_malloc.h

--- progetto SO/pseudo_malloc.h
+++ progetto SO/pseudo_malloc.h
@@ -3,9 +3,11 @@
 
 #define PAGE_SIZE 4096              //misura delle pagine più comunemente usata
 #define MAX_BUDDY_SIZE 1024*1024    //(1MB)
+#define B_LEVELS 9                  // visto negli esempi (source) del corso
+#define MIN_BUDDY_SIZE (MAX_BUDDY_SIZE >> B_LEVELS) //((MAX_BUDDY_SIZE/2)^B_LEVEL = 2KB) valore più piccolo che il buddy allocator può gestire.
 
 //inizializzazione dell'allocatore
-void pseudoMalloc_init(buddy_allocator * b_alloc, int n_levels, int min_size, uint8_t buf);
+void pseudoMalloc_init(buddy_allocator * b_alloc);
 
 //alloca la memoria
 void * pseudoMalloc_alloc(buddy_allocator * b_alloc, int size);
